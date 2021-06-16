9b87b493060efbec53d31f5d1aa69a1b0e537e6c
MinchinWeb <w_minchin@hotmail.com>
2021-06-15 22:39:34 -0600

    Prep writer for merging of https://github.com/getpelican/pelican/pull/2882

--- pelican/plugins/xml_writer/writer.py
+++ pelican/plugins/xml_writer/writer.py
@@ -3,6 +3,7 @@ from posixpath import join as posix_join
 from urllib.parse import urljoin
 
 from pelican.writers import Writer
+from .constants import LOG_PREFIX
 
 logger = logging.getLogger(__name__)
 
@@ -11,16 +12,27 @@ class XMLWriter(Writer):
     """Pelican writer, extended to output XML files."""
 
     def __init__(self, output_path, settings=None):
-        self.output_path = output_path
-        self.reminder = dict()
-        self.settings = settings or {}
-        self._written_files = set()
-        self._overridden_files = set()
+        logger.debug("%s XMLWriter initialized" % LOG_PREFIX)
+        try:
+            super().__init__(output_path, settings=settings)
+        except KeyError:
+            # c.f https://github.com/getpelican/pelican/pull/2882
+            self.output_path = output_path
+            self.reminder = dict()
+            self.settings = settings or {}
+            self._written_files = set()
+            self._overridden_files = set()
 
-        # See Content._link_replacer for details
-        if "RELATIVE_URLS" in self.settings and self.settings["RELATIVE_URLS"]:
-            self.urljoiner = posix_join
-        else:
-            self.urljoiner = lambda base, url: urljoin(
-                base if base.endswith("/") else base + "/", url
-            )
+            # See Content._link_replacer for details
+            if "RELATIVE_URLS" in self.settings and self.settings["RELATIVE_URLS"]:
+                self.urljoiner = posix_join
+            else:
+                self.urljoiner = lambda base, url: urljoin(
+                    base if base.endswith("/") else base + "/", url
+                )
+
+    def write_xml(self, xml, output_path):
+        pass
+
+    def write_heatmap(self, xml, output_path):
+        pass
