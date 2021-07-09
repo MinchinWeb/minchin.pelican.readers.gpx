e8aaeddf6c361e762af5143cb0dd0629757b6f0b
MinchinWeb <w_minchin@hotmail.com>
2021-07-08 23:31:06 -0600

    Make writer findable by Pelican
    See https://github.com/getpelican/pelican/pull/2899#issuecomment-876662852

--- pelican/plugins/everything_writer/__init__.py
+++ pelican/plugins/everything_writer/__init__.py
@@ -4,6 +4,10 @@ from .constants import __version__
 from .writer import EverythingWriter
 
 
+def add_writer(pelican):
+    return EverythingWriter
+
+
 def register():
     """Register the plugin pieces with Pelican."""
-    pelican_signals.get_writer.connect(EverythingWriter)
+    pelican_signals.get_writer.connect(add_writer)
